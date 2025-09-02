e9e382c0379bbb89f117d9e9fb7a3636dea2041b
madgt <marcelodgt@gmail.com>
2025-09-01 22:12:15 -0300

    adding tailwind to the project

--- frontend/src/components/Home/home.tsx
+++ frontend/src/components/Home/home.tsx
@@ -19,9 +19,24 @@ export default function Home() {
     <div>
       <h1>Welcome to the Home Page</h1>
       <p>This is the main landing page of the application.</p>
-      <button onClick={gotoCategories}>Go to Categories</button>
-      <button onClick={gotoTransactions}>Go to Transactions</button>
-      <button onClick={goToCategoryTypes}>Go to Category Types</button>
+      <button
+        className="mt-4 rounded-xl px-4 py-2 bg-blue-600 text-white hover:bg-blue-700"
+        onClick={gotoCategories}
+      >
+        Go to Categories
+      </button>
+      <button
+        className="mt-4 rounded-xl px-4 py-2 bg-blue-600 text-white hover:bg-blue-700"
+        onClick={gotoTransactions}
+      >
+        Go to Transactions
+      </button>
+      <button
+        className="mt-4 rounded-xl px-4 py-2 bg-blue-600 text-white hover:bg-blue-700"
+        onClick={goToCategoryTypes}
+      >
+        Go to Category Types
+      </button>
     </div>
   );
 }
